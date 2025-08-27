dae2c4b486c4624762f331e770458b7cccd02c88
kubwimanav <kubwimanavincent2020@gmail.com>
2025-08-27 11:30:30 +0200

    vercel

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -4,7 +4,7 @@ import ProductList from "../components/DBProducts/ProductList";
 import type { Product } from "../types/productTypes";
 
 export default function ProductPage() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
@@ -13,15 +13,11 @@ export default function ProductPage() {
       .catch((err) => console.error("Error fetching products:", err));
   }, []);
 
-  const handleProductAdded = (newProduct: Product) => {
-    // ✅ Immediately add it to the list
-    setProducts((prev) => [...prev, { ...newProduct, id: prev.length + 1 }]);
-  };
+  
 
   return (
     <div className="">
-      {/* <AddProduct onProductAdded={handleProductAdded} /> */}
-      <ProductList products={products} />
+      <ProductList refreshKey={0} />
     </div>
   );
 }
